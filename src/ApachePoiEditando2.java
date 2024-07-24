7f0954804ef44e63b2d93f2008159bd4cd7c226d
Leno rafael <73203800+lenors@users.noreply.github.com>
2024-07-24 09:08:05 +0100

    Mensae

--- src/ApachePoiEditando2.java
+++ src/ApachePoiEditando2.java
@@ -3,10 +3,13 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Iterator;
 
-public class ApachePoiEditando {
+public class ApachePoiEditando2 {
     public static void main(String[] args) throws IOException {
         File file = new File("C:\\Users\\Lenors\\IdeaProjects\\Arquivos\\arquivo_excel.xls");
 
